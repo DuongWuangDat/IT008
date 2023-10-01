55c36e6ddf1917e739f4b9c7e42d185625931ad7
Dat Duong <duongquangdat172004@gmail.com>
2023-10-01 20:44:06 +0700

    sua 1.4 event chuan .NET

--- Lab1/bai_1.4/Program.cs
+++ Lab1/bai_1.4/Program.cs
@@ -2,28 +2,26 @@
 
 namespace Program
 {
-    public delegate void UpdateNhietKeHandler(double temp);
     public class main
     {
         public static void Main(string[] args)
         {
             NhietKe temp = new NhietKe();
             Random rnd = new Random();
-            temp.UpdateNhietKe += Temp_UpdateNhietKe;
+            temp.NhietKeChanged += Temp_NhietKeChanged;
             for(int i=0; i< 10; i++)
             {
                 temp.nhietKe = Math.Round((rnd.NextDouble() + rnd.Next(-80, 80)), 2);
             }
         }
 
-        private static void Temp_UpdateNhietKe(double temp)
+        private static void Temp_NhietKeChanged(object? sender, NhietKeChangedArgs e)
         {
-            Console.WriteLine("Nhiet do moi la: " +  temp);
+            Console.WriteLine("Nhiet do moi la: " + e.nhietDo);
         }
     }
     public class NhietKe
     {
-        public event UpdateNhietKeHandler UpdateNhietKe;
         private double _nhietKe;
 
         public double nhietKe
@@ -31,9 +29,29 @@ namespace Program
             get { return _nhietKe; }
             set { 
                 _nhietKe = value;
-                UpdateNhietKe(nhietKe);
+                OnNhietKeChange(value);
             }
         }
-
+        private event EventHandler<NhietKeChangedArgs> _nhietKeChanged;
+        public event EventHandler<NhietKeChangedArgs> NhietKeChanged
+        {
+            add { _nhietKeChanged += value;}
+            remove { _nhietKeChanged -= value;}
+        }
+        void OnNhietKeChange(double nhietKe)
+        {
+            if(_nhietKeChanged != null)
+            {
+                _nhietKeChanged(this, new NhietKeChangedArgs(nhietKe));
+            }
+        }
+    }
+    public class NhietKeChangedArgs : EventArgs
+    {
+        public double nhietDo { get; set; }
+        public NhietKeChangedArgs(double nhietDo)
+        {
+            this.nhietDo = nhietDo; 
+        }
     }
 }

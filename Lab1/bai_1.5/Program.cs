7dea7984718b6ac0fed98ad7bb214b1f371d5bc1
Dat Duong <duongquangdat172004@gmail.com>
2023-10-01 12:53:08 +0700

    finish 1.5

---
+++ Lab1/bai_1.5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+namespace Program
+{
+    public class main
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhap duong dan thu muc: ");
+            string path= Console.ReadLine();
+           
+            DirectoryInfo directory = new DirectoryInfo(path);
+            if (!directory.Exists)
+            {
+                Console.WriteLine("Thu muc khong ton tai");
+                return;
+            }
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                Console.WriteLine(file.Name);
+            }
+        }
+    }
+}

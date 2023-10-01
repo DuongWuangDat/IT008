fcbc952decd78f242584a45e2af14e81a44360ba
Dat Duong <duongquangdat172004@gmail.com>
2023-10-01 12:13:07 +0700

    first commit

--- Lab1/bai_1.4/Program.cs
+++ Lab1/bai_1.4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Program
+{
+    public class main
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}

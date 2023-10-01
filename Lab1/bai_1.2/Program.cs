f661587d047a9f10c6e7c7c1db23df0311fec1c0
Nguyen Duong <nduongg04@gmail.com>
2023-10-01 15:20:38 +0700

    Update bai_1.2

--- Lab1/bai_1.2/Program.cs
+++ Lab1/bai_1.2/Program.cs
@@ -1,9 +1,69 @@
 ﻿using System;
+using nPhanSo;
 
+namespace Program
+{
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Nhập phép tính: ")
+            try
+            {
+                Console.Write("Nhap phep tinh (vd: 1/2 + 1/2): ");
+                string input = Console.ReadLine();
+                string[] phepTinh = input.Split(' ');
+                PhanSo a = PhanSo.ConVertToPhanSo(phepTinh[0]);
+                PhanSo b = PhanSo.ConVertToPhanSo(phepTinh[2]);
+                string printedString;
+                switch (phepTinh[1])
+                {
+                    case "+":
+                        printedString = (a + b).ToString();
+                        break;
+                    case "-":
+                        printedString = (a - b).ToString();
+                        break;
+                    case "*":
+                        printedString = (a * b).ToString();
+                        break;
+                    case "/":
+                        printedString = (a / b).ToString();
+                        break;
+                    case "==":
+                        if (a == b)
+                            printedString = "Dung";
+                        else
+                            printedString = "Sai";
+                        break;
+                    case "!=":
+                        if (a != b)
+                            printedString = "Dung";
+                        else
+                            printedString = "Sai";
+                        break;
+                    case "<":
+                        if (a < b)
+                            printedString = "Dung";
+                        else
+                            printedString = "Sai";
+                        break;
+                    case ">":
+                        if (a > b)
+                            printedString = "Dung";
+                        else
+                            printedString = "Sai";
+                        break;
+                    default:
+                        printedString = "Toan tu ban nhap vao khong dung!";
+                        break;
+                }
+                Console.WriteLine(printedString);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
+}

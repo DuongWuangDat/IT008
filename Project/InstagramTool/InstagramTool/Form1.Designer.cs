8eb2263e252d3f0f1c9e8a86c8d6439c93554d50
Dat Duong <duongquangdat172004@gmail.com>
2023-11-23 12:35:41 +0700

    update auto tim

--- Project/InstagramTool/InstagramTool/Form1.Designer.cs
+++ Project/InstagramTool/InstagramTool/Form1.Designer.cs
@@ -28,13 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.username_box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.password_box = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.autotimbtn = new System.Windows.Forms.Button();
+            this.autocmtbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.Location = new System.Drawing.Point(88, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(254, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Log in";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // username_box
+            // 
+            this.username_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username_box.Location = new System.Drawing.Point(222, 38);
+            this.username_box.Name = "username_box";
+            this.username_box.Size = new System.Drawing.Size(268, 30);
+            this.username_box.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(99, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            // 
+            // password_box
+            // 
+            this.password_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password_box.Location = new System.Drawing.Point(222, 94);
+            this.password_box.Name = "password_box";
+            this.password_box.Size = new System.Drawing.Size(268, 30);
+            this.password_box.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(99, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button2.Location = new System.Drawing.Point(412, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(254, 61);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Open";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // autotimbtn
+            // 
+            this.autotimbtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.autotimbtn.Location = new System.Drawing.Point(88, 234);
+            this.autotimbtn.Name = "autotimbtn";
+            this.autotimbtn.Size = new System.Drawing.Size(254, 61);
+            this.autotimbtn.TabIndex = 6;
+            this.autotimbtn.Text = "AutoTim";
+            this.autotimbtn.UseVisualStyleBackColor = false;
+            this.autotimbtn.Click += new System.EventHandler(this.autotimbtn_Click);
+            // 
+            // autocmtbtn
+            // 
+            this.autocmtbtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.autocmtbtn.Location = new System.Drawing.Point(412, 234);
+            this.autocmtbtn.Name = "autocmtbtn";
+            this.autocmtbtn.Size = new System.Drawing.Size(254, 61);
+            this.autocmtbtn.TabIndex = 7;
+            this.autocmtbtn.Text = "AutoComment";
+            this.autocmtbtn.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.autocmtbtn);
+            this.Controls.Add(this.autotimbtn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.password_box);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.username_box);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox username_box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox password_box;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button autotimbtn;
+        private System.Windows.Forms.Button autocmtbtn;
     }
 }
 
--- Project/InstagramTool/InstagramTool/Form1.cs
+++ Project/InstagramTool/InstagramTool/Form1.cs
@@ -1,20 +1,66 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Xml.Linq;
 namespace InstagramTool
 {
     public partial class Form1 : Form
     {
+        IWebDriver driver;
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.instagram.com/");
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            IWebElement user = driver.FindElement(By.Name("username"));
+            IWebElement pass = driver.FindElement(By.Name("password"));
+            IWebElement login = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/div/div[3]/button"));
+            user.SendKeys(username_box.Text);
+            pass.SendKeys(password_box.Text);
+            Thread.Sleep(2000);
+            login.Click();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            driver.Navigate().GoToUrl("https://www.instagram.com/taylorswift/");
+            IWebElement divElement = driver.FindElement(By.XPath("/html/body/div[2]"));
+            string userID = divElement.GetAttribute("id");
+            IWebElement selectPost = driver.FindElement(By.XPath("//*[@id=\"" + userID + "\"]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/div[3]/article/div[1]/div/div[1]/div[1]/a"));
+            selectPost.Click();
+            
+        }
+
+        private void autotimbtn_Click(object sender, EventArgs e)
+        {
+            IWebElement nextButton = driver.FindElement(By.XPath("/html/body/div[7]/div[1]/div/div[3]/div/div/div/div/div[1]/div/div/div/button"));
+            IWebElement timButton = driver.FindElement(By.XPath("/html/body/div[7]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[1]/span[1]/div"));
+            while (nextButton.Displayed)
+            {
+                timButton.Click();
+                Thread.Sleep(2000);
+                nextButton.Click();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+                nextButton = driver.FindElement(By.XPath("/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div[1]/div/div/div[2]/button"));
+                timButton = driver.FindElement(By.XPath("/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[1]/span[1]/div"));
+            }
         }
     }
 }

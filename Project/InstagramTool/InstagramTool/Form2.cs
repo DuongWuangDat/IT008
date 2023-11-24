c20034099ab4771cff4097e7b6ea5b2dc455d415
duc190604 <haminhduchaminhduc@gmail.com>
2023-11-25 04:13:16 +0700

    update_auto_cmt

--- Project/InstagramTool/InstagramTool/Form2.cs
+++ Project/InstagramTool/InstagramTool/Form2.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
 
 namespace InstagramTool
 {
@@ -17,6 +18,7 @@ namespace InstagramTool
     {
         private Form1 f1;
         IWebDriver driver;
+        private Random random;
         private string user;
         private string pass;
         private List<stringitem> ListUser;
@@ -28,8 +30,8 @@ namespace InstagramTool
             ListUser = new List<stringitem>();
             ListCmt = new List<stringitem>();
             user = a;
-            pass = b;   
-           
+            pass = b;
+
         }
 
         private void Form2_FormClosed(object sender, FormClosedEventArgs e)
@@ -39,7 +41,7 @@ namespace InstagramTool
 
         private void adduser_Click(object sender, EventArgs e)
         {
-            ListUser.Add(new stringitem { text=userbox.Text});
+            ListUser.Add(new stringitem { text = userbox.Text });
             userbox.Text = string.Empty;
             griduser.DataSource = null;
             griduser.DataSource = ListUser;
@@ -47,11 +49,12 @@ namespace InstagramTool
 
         private void addcmt_Click(object sender, EventArgs e)
         {
-            ListCmt.Add(new stringitem { text=cmtbox.Text});
+            ListCmt.Add(new stringitem { text = cmtbox.Text });
             cmtbox.Text = string.Empty;
             gridcmt.DataSource = null;
             gridcmt.DataSource = ListCmt;
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -66,6 +69,102 @@ namespace InstagramTool
             pass1.SendKeys(pass);
             Thread.Sleep(2000);
             login.Click();
+            Thread.Sleep(4000);
+            //
+            foreach (stringitem url in ListUser)
+            {
+
+                driver.Navigate().GoToUrl(url.text);
+                Thread.Sleep(2000);
+                bool isEndOfPage = false;
+                int x = 0;
+                while (!isEndOfPage)
+                {
+                    int y = 0;
+                    IList<IWebElement> listpost = driver.FindElements(By.XPath("//article//a"));
+                    foreach (var post in listpost)
+                    {
+                        y++;
+                        if (y >= x)
+                        {
+                            x++;
+                            post.Click();
+                            Thread.Sleep(2000);
+                            random = new Random();
+                            int i = random.Next(0, ListCmt.Count);
+                            string comment = ListCmt[i].text;
+                            try
+                            {
+                                var commentbox = driver.FindElement(By.XPath("/html/body/div[7]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/textarea"));
+                                Thread.Sleep(1000);
+                                commentbox.SendKeys(comment);
+                            }
+                            catch
+                            {
+                                try
+                                {
+                                    var commentbox = driver.FindElement(By.XPath("/html/body/div[7]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/textarea"));
+                                    Thread.Sleep(1000);
+                                    commentbox.SendKeys(comment);
+                                }
+                                catch
+                                {
+                                    try
+                                    {
+                                        var commentbox = driver.FindElement(By.XPath("/html/body/div[8]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/textarea"));
+                                        Thread.Sleep(1000);
+                                        commentbox.SendKeys(comment);
+                                    }
+                                    catch
+                                    {
+                                        var commentbox = driver.FindElement(By.XPath("/html/body/div[8]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/textarea"));
+                                        Thread.Sleep(1000);
+                                        commentbox.SendKeys(comment);
+                                    }
+
+
+                                }
+                            }
+
+                            Thread.Sleep(500);
+                            try
+                            {
+                                var btnpost = driver.FindElement(By.XPath("/html/body/div[7]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/div[2]"));
+                                btnpost.Click();
+                                Thread.Sleep(2000);
+                            }
+                            catch
+                            {
+                                var btnpost = driver.FindElement(By.XPath("/html/body/div[8]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/div[2]"));
+                                btnpost.Click();
+                                Thread.Sleep(2000);
+                            }
+
+                            driver.Navigate().Back();
+                            Thread.Sleep(2000);
+                        }
+                       
+                    
+
+                    }
+                    try
+                    {
+                        // Cuộn trang xuống dưới (đây là một ví dụ, bạn có thể cần điều chỉnh cho phù hợp)
+                        IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+                        js.ExecuteScript("window.scrollTo(0, document.body.scrollHeight)");
+
+                        // Đợi một khoảng thời gian trước khi lấy các phần tử tiếp theo
+                        Thread.Sleep(1000); // Hoặc có thể sử dụng WebDriverWait để đợi sự kiện cụ thể
+                    }
+                    catch (Exception)
+                    {
+                        isEndOfPage = true; // Đánh dấu kết thúc trang khi không thể cuộn nữa
+                    }
+
+
+
+                }
+            }
         }
     }
     public  class stringitem
@@ -73,3 +172,4 @@ namespace InstagramTool
         public string text { get; set; }
     }
 }
+
